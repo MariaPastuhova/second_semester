a15c8c92c4408488951c97357c3a7b24b651e976
MariaPastuhova <masapastuhova777@gmail.com>
2025-02-14 15:21:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+extern int Sum(int a, int b);
+extern int Mult(int a, int b);
+
+int main()
+{
+	int a = 5;
+	int b = 8;
+
+	int c = Sum(5, 4);
+	int d = Mult(2, 7);
+
+	std::cout << c;
+
+	std::cout << " " << d;
+}
